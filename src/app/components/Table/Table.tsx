0292a25d0a48a43fb6bfa8cd6fb92ae0d46eaed1
Milaud Nik-Ahd <milaud@Mac.attlocal.net>
2025-08-19 17:36:37 -0700

    add curved pass line

--- src/app/components/Table/Table.tsx
+++ src/app/components/Table/Table.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import styles from "./Table.module.css";
-import { Come, DontComeBar, DontPassBar, Field, PointCells } from "../Cell/Cell";
+import { Come, DontComeBar, DontPassBar, Field, PassLine, PointCells } from "../Cell/Cell";
 
 
 export const Table: React.FC = () => {
 
     return (
         <div className={styles.table}>
+            <div><PassLine /></div>
             <div><DontPassBar /></div>
             <div className={styles.topRowContainer}>
                 <div className={styles.topRow}>
