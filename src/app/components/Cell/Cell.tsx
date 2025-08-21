eded7af2f34c2e2f16dc15ab04d8b8b34d6e99b1
Milaud Nik-Ahd <milaud@Mac.attlocal.net>
2025-08-20 23:22:09 -0700

    changes

--- src/app/components/Cell/Cell.tsx
+++ src/app/components/Cell/Cell.tsx
@@ -3,7 +3,8 @@ import styles from "./Cell.module.css";
 
 
 interface CellProps {
-    id: string; 
+    style?: string,
+    id: string;
     displayName: string
     onPlaceChip?: (key: string) => void;
     totalBet?: number;
@@ -14,7 +15,7 @@ const placeChip = (id: string) => {
     console.log(id)
 };
 
-export const Cell: React.FC<CellProps> = ({ id, displayName, onPlaceChip, totalBet, winningNumber }) => {
+export const Cell: React.FC<CellProps> = ({ style, id, displayName, onPlaceChip, totalBet, winningNumber }) => {
     const isWinning = id === winningNumber;
     // Map total bet to chip color if matches 5,25,100; else default white
     const chipColor =
@@ -23,78 +24,110 @@ export const Cell: React.FC<CellProps> = ({ id, displayName, onPlaceChip, totalB
                 totalBet === 100 ? "#d4af37" :
                     totalBet ? "#890fdbff" : "";
 
+    // className={`${styles.cell} ${isWinning ? styles.highlight : ""}`}
+
     return (
         <div
-            className={`${styles.cell} ${isWinning ? styles.highlight : ""}`}
+            className={`${style} ${isWinning ? styles.highlight : ""}`}
             title={`${id}`}
             onClick={() => placeChip(id)}
         >
-            <div>
-                <span>{displayName}</span>
-            </div>
-
+            {/* {displayName} */}
+            <span className={styles.cellSpan}>{displayName}</span>
             {/* {totalBet && totalBet > 0 && (
-        <div className={styles.cellBet}>
-          <div className={styles.chip} style={{ backgroundColor: chipColor }}>
-            {totalBet}
-          </div>
-        </div>
-      )} */}
+                <div className={styles.cellBet}>
+                    <div className={styles.chip} style={{ backgroundColor: chipColor }}>
+                        {totalBet}
+                    </div>
+                </div>
+            )} */}
         </div>
     );
 };
 
-export const DontComeBar: React.FC= () => {
+export const PointCell: React.FC<CellProps> = ({
+    id,
+    displayName
+}) => {
     return (
-        <div className={styles.dontComeBarCell}>
-            <Cell id="dontComeBar" displayName="DON'T COME BAR"/>
+        <div className={styles.cell}>
+            <Cell id={id} displayName={displayName} />
+        </div>
+        
+    );
+};
+
+export const PointCells: React.FC = () => {
+    return (
+        <div className={styles.pointCells}>
+            {/* <PointCell id="4" displayName="4" />
+            <PointCell id="5" displayName="5" />
+            <PointCell id="6" displayName="Six" />
+            <PointCell id="8" displayName="8" />
+            <PointCell id="9" displayName="Nine" />
+            <PointCell id="10" displayName="10" /> */}
+            <Cell style={styles.cell} id="4" displayName="4" />
+            <Cell style={styles.cell}  id="5" displayName="5" />
+            <Cell style={styles.cell}  id="6" displayName="Six" />
+            <Cell style={styles.cell}  id="8" displayName="8" />
+            <Cell style={styles.cell}  id="9" displayName="Nine" />
+            <Cell style={styles.cell}  id="10" displayName="10" />
+        </div>
+    );
+};
+
+export const DontComeBar: React.FC = () => {
+    return (
+        <div className={styles.dontComeBar}>
+            <Cell id="dontComeBar" displayName="DON'T COME BAR" />
         </div>
     );
 };
 
 export const DontPassBar: React.FC = () => {
     return (
-        <div className={styles.dontPassBarCell}>
-            <Cell id="dontPassBar" displayName="DON'T PASS BAR" />
-        </div>
+        <Cell style={styles.dontPassBar} id="dontPassBar" displayName="DON'T PASS BAR" />
     );
 };
 
 export const PassLine: React.FC = () => {
     return (
-        <div className={styles.passLineCell}>
-            <Cell id="passLine" displayName="PASS LINE" />
-        </div>
+        <Cell style={styles.passLine} id="passLine" displayName="PASS LINE" />
     );
 };
 
-
-export const PointCells: React.FC = () => {
+export const PassLines: React.FC = () => {
     return (
-        <div className={styles.pointCells}>
-            <Cell id="4" displayName="4" />
-            <Cell id="5" displayName="5" />
-            <Cell id="6" displayName="Six" />
-            <Cell id="8" displayName="8" />
-            <Cell id="9" displayName="Nine" />
-            <Cell id="10" displayName="10" />
+        <div className={styles.outerPassLine}>
+            <PassLine />
+            <DontPassBar />
+        </div>
+        
+    );
+};
+
+export const ComeField: React.FC = () => {
+    return (
+        <div className={styles.comeFieldContainer}>
+            <div className={styles.comeFieldCell}>
+                <Cell id="come" displayName="COME" />
+            </div>
+            <div className={styles.comeFieldCell}>
+                <Cell id="field" displayName="2 3·4·9·10·11 12 FIELD" />
+            </div>
         </div>
     );
 };
 
 export const Come: React.FC = () => {
     return (
-        <div className={styles.comeCell}>
-            <Cell id="come" displayName="COME" />
-        </div>
+        <Cell style={styles.comeFieldCell} id="come" displayName="COME" />
     );
 };
 
 export const Field: React.FC = () => {
     return (
-        <div className={styles.fieldCell}>
-            <Cell id="field" displayName="2 3·4·9·10·11 12 FIELD" />
-        </div>
+        <Cell style={styles.comeFieldCell} id="field" displayName="2 3·4·9·10·11 12 FIELD" />
     );
 };
 
--- src/app/components/Table/Table.tsx
+++ src/app/components/Table/Table.tsx
@@ -2,26 +2,24 @@
 
 import React, { useState } from "react";
 import styles from "./Table.module.css";
-import { Come, DontComeBar, DontPassBar, Field, PassLine, PointCells } from "../Cell/Cell";
+import { Come, DontComeBar, DontPassBar, Field, PassLine, PassLines, PointCells, ComeField } from "../Cell/Cell";
 
 
 export const Table: React.FC = () => {
 
     return (
-        <div className={styles.table}>
+        <div className={styles.container}>
             <div className={styles.outerContainer}>
-                <PassLine />
-            </div>
-            <div className={styles.innerContainer}>
-                <DontPassBar />
-                <div className={styles.topRowContainer}>
-                    <div className={styles.topRow}>
-                        <DontComeBar />
-                        <PointCells />
+                <div className={styles.innerContainer}>
+                    <DontComeBar />
+                    <PointCells />
+                    <div className={styles.comeFieldContainer}>
+                        <Come />
+                        <Field />
                     </div>
-                    <Come />
-                    <Field />
                 </div>
+                <PassLines />
+
             </div>
         </div>
     );
